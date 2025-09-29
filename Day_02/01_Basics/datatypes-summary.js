4dffa0e17977519cfc9fc7425ff376df64d987f4
MaitrikKaushik <maitrik2006.kaushik.morni@gmail.com>
2025-09-29 12:34:20 +0530

    [200~feat(js): add lessons on type conversions, comparison operators, and datatype summary ~

---
+++ Day_02/01_Basics/datatypes-summary.js
@@ -0,0 +1,38 @@
+// Primitive
+
+// 7 types : String, Number, boolean, null, undefined, symbol, BigInt
+
+// const score = 100.3;
+
+// const isLoggedin = false;
+// const outsideTemp = null;
+// let userEmail;
+
+// const id = Symbol('123')
+// const anotherId = Symbol('123')
+
+
+// console.log(id === anotherId);
+
+// const bigNumber = 3465436597777777n
+
+
+// Reference (Non primitive)
+
+// Array, Object, Function == data type is returned as object.
+
+const heros = ["Shaktiman", "NaagRaj", "Doga"]
+let myObj = {
+    name: "Maitrik",
+    age: 22,
+}
+
+const myFunction = function(){
+    console.log("Hello Charlie!! ");
+    
+}
+
+console.log(typeof myObj);
+
+
+
